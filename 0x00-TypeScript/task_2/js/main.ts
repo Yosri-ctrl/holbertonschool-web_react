79467d5af4b0fda1831f4d9903ff2e59d519fb63
Yurei <yosribouabid@gmail.com>
2021-07-25 20:07:11 +0100

    task 5

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,47 @@
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[propName: string]: any;
+}
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+	return `${firstName[0]}. ${lastName}`;
+}
+console.log(printTeacher('John', 'Doe'));
+export default printTeacher;
+
+interface classInterface {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+interface classConstructor {
+	new(firstName: string, lastName: string): classInterface;
+}
+
+class StudentClass implements classInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return "Currently working";
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
